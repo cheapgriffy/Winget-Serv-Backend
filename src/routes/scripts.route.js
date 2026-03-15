4f8b109c70b066fe49260e94ed06c02249f610bd
Griffy <timeo.felix.aux@gmail.Com>
2026-03-15 19:38:15 +0100

    fix: README update & script route comments

--- src/routes/scripts.route.js
+++ src/routes/scripts.route.js
@@ -2,10 +2,19 @@ const router = require("express").Router()
 const scriptsController = require("../controllers/script.controller")
 const auth = require("../middlewares/auth")
 
+    // Temrinal execution + Browser view
 router.get("/:public_id", scriptsController.getScript)
-router.post("/create", auth, scriptsController.createScript)
-router.delete("/remove", auth, scriptsController.deleteScript)
+
+    // get a list of all user scripts
 router.get("/list", auth, scriptsController.getAllUserScript)
+
+    // Create new script, 
+    //* { "name", "description", "content":ARRAY }
+router.post("/create", auth, scriptsController.createScript)
+
+    // Delete a script that is owned by user
+    //* { "id":INT }
+router.delete("/remove", auth, scriptsController.deleteScript)
 
 
 module.exports = router
