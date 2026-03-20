6ffa6c1e4e186e44dd36cce1697d57a8092e2c2f
Griffy <timeo.felix.aux@gmail.com>
2026-03-20 15:11:10 +0100

    feat: Added CORS support
    Cors is mandatory to be implemented for deployability online, cause all the test were made on the same machine with same ip and target

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const cors = require("cors")
 
 app.use(express.json())
 
@@ -9,6 +10,9 @@ const launchParams = require('./src/config/launch.params').configVariables
 const configDB = require('./src/config/db')
 const scriptRoute = require('./src/routes/scripts.route')
 const userRoute = require('./src/routes/user.route')
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors())
 
 // runned here to prevent circular dependency
 if (launchParams.INIT_DB == true) {
