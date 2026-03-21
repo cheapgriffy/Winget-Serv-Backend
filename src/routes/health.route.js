386f8d158093c9f195a753a8c2f029c4f893c913
Griffy <timeo.felix.aux@gmail.com>
2026-03-21 12:28:44 +0100

    feat: Added current "quirck" in api health broadcast

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -1,9 +1,24 @@
 const router = require("express").Router()
+const configVariables = require("../config/launch.params").configVariables
+
 
 router.get("/", (req, res, next) => {
+    // Quick contain public info of parameters
+    // ex, if account creation is disabled
+    let quircks = []
+    if(configVariables.NO_ACCOUNT_CREATION){
+        quircks.push("Account Creation has been disabled by the administrator")
+    }
+    
+    if(quircks.length === 0){
+        quircks.push("No quicks has been found")
+        quircks.push("Running default configuration")
+    }
+
     try{
         res.status(200).json({
-            "message": "API is ONLINE"
+            "message": "API is ONLINE",
+            quircks
         })
     } catch(err){
         next(err)
